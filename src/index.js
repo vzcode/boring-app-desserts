5008f6867ea14d16863986bb5a33f4179dfc54df
kevz <vzcodesthings@gmail.com>
2019-06-16 20:38:34 -0700

    Adding CORS for S3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 'use strict';
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const AWSXray = require('aws-xray-sdk');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const PORT = process.env.PORT || 8081;
 app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://bas-c.s3-website-us-west-1.amazonaws.com'
+}));
 app.use(AWSXray.express.openSegment('Dessert Service'));
 app.use('/', routes);
 app.use(AWSXray.express.closeSegment());
-// app.use('/desserts', routes);
-// app.use('/add-dessert', routes);
 
 app.listen(PORT);
 console.log(`Running on http://${PORT}`);
